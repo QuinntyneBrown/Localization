52ff68ee231a4fbf840aca01ec3aa7b26e8cf60c
QuinntyneBrown <quinntynebrown@gmail.com>
2021-04-30 07:06:51 -0400

    update

--- src/Localization.App/src/app/contacts/contact-detail-intl.ts
+++ src/Localization.App/src/app/contacts/contact-detail-intl.ts
@@ -1,34 +1,16 @@
 import { Injectable } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
-import { of, merge, BehaviorSubject } from "rxjs";
-import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class ContactDetailIntl {
-    public editContactLabel$: BehaviorSubject<string> = new BehaviorSubject("Contact List");
-    public createContactLabel$: BehaviorSubject<string> = new BehaviorSubject("Create");
+    public editContactLabel$: Observable<string> = this._translateService.stream("CONTACTS.EDIT_CONTACT");
+
+    public createContactLabel$: Observable<string> = this._translateService.stream("CONTACTS.CREATE_CONTACT");
 
     constructor(
-        private readonly _translateService: TranslateService) {
-    
-        merge(of(true), this._translateService.onLangChange).pipe(
-          tap(x => this.getAndInitTranslations())
-        ).subscribe();
-    }
-
-    getAndInitTranslations() {
-        this._translateService.get([
-            "CONTACTS.CREATE_CONTACT",
-            "CONTACTS.EDIT_CONTACT"
-          ])
-          .pipe(
-              tap(translations => {
-                this.editContactLabel$.next(translations["CONTACTS.EDIT_CONTACT"]),
-                this.createContactLabel$.next(translations["CONTACTS.CREATE_CONTACT"])
-              })
-          )
-          .subscribe();
+        private readonly _translateService: TranslateService) {  
     }
 }
--- src/Localization.App/src/app/contacts/contact-list-intl.ts
+++ src/Localization.App/src/app/contacts/contact-list-intl.ts
@@ -1,36 +1,17 @@
 import { Injectable } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
-import { of, merge, BehaviorSubject } from "rxjs";
+import { of, merge, BehaviorSubject, Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
 @Injectable({
     providedIn: "root"
 })
 export class ContactListIntl {
-    public titleLabel$: BehaviorSubject<string> = new BehaviorSubject("Contact List");
-    public createLabel$: BehaviorSubject<string> = new BehaviorSubject("Create");
-    public nameLabel$: BehaviorSubject<string> = new BehaviorSubject("Name");
+    public titleLabel$: Observable<string> = this._translateService.stream("CONTACTS.LIST");
+    public createLabel$: Observable<string> = this._translateService.stream("COMMON.CREATE");
+    public nameLabel$: Observable<string> = this._translateService.stream("COMMON.NAME");
 
     constructor(
         private readonly _translateService: TranslateService) {
-        merge(of(true), this._translateService.onLangChange).pipe(
-          tap(x => this.getAndInitTranslations())
-        ).subscribe();
-    }
-
-    getAndInitTranslations() {
-        this._translateService.get([
-            "CONTACTS.LIST",
-            "COMMON.CREATE",
-            "COMMON.NAME"
-          ])
-          .pipe(
-              tap(translations => {
-                this.titleLabel$.next(translations["CONTACTS.LIST"]),
-                this.createLabel$.next(translations["COMMON.CREATE"]),
-                this.nameLabel$.next(translations["COMMON.NAME"])
-              })
-          )
-          .subscribe();
     }
 }
